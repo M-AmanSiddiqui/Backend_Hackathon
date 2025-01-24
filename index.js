36471f53f3f8fba655c4f53df47e35f9678680c4
M-Aman Siddiqui <m.amansiddiqui2024@gmail.com>
2025-01-24 14:16:23 -0800

    first api bn gya

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello world")
+import express from "express";
+const app = express();
+const PORT = 4000;
+
+app.get('/',(req , res)=> {
+    console.log(req)
+    res.send("hello world")
+})
+app.listen(PORT , () => console.log("Server is runing on port"+ PORT));
