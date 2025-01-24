41486a84b25361e6e1475b3dec969610a4520d33
M-Aman Siddiqui <m.amansiddiqui2024@gmail.com>
2025-01-24 14:51:03 -0800

    add object

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
+const tasks = [
+    {
+        id : 1,
+        task : "sona nhi hai"
+    },
+    {
+        id : 2,
+        task : "jagna hai"
+    },
+    {
+        id : 3,
+        task : "parhna hai"
+    }
+]
+
 import express from "express";
 const app = express();
 const PORT = 4000;
 
 app.get('/',(req , res)=> {
-    console.log(req)
-    res.send("hello world")
-})
+    // console.log(req)
+    res.status(200).send(tasks)
+});
+
+app.post('/',(req , res)=> {
+    // console.log(req)
+    res.send("POST REQUEST")
+});
+
+app.put('/',(req , res)=> {
+    // console.log(req)
+    res.send("PUT REQUEST")
+});
+
+
+app.delete('/',(req , res)=> {
+    // console.log(req)
+    res.send("DeLETE REQUEST")
+});
+
 app.listen(PORT , () => console.log("Server is runing on port"+ PORT));
